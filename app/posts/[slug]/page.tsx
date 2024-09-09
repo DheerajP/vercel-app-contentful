136d3cfdb78b9515cc5fa3940fb8231639be7474
dheerajContentful <dheeraj.palagiri@contentful.com>
2024-09-09 17:32:46 -0500

    updates

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -23,7 +23,7 @@ export default async function PostPage({
   params: { slug: string };
 }) {
   const { isEnabled } = draftMode();
-  const { post, morePosts } = await getPostAndMorePosts(params.slug, isEnabled);
+  const { post, morePosts } = await getPostAndMorePosts(params.slug, true);
 
   return (
     <div className="container mx-auto px-5">
