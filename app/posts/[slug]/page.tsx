f53768e589d9301cbd1006715f23aa229dfbc41e
dheerajContentful <dheeraj.palagiri@contentful.com>
2024-09-09 18:03:58 -0500

    updates

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -10,7 +10,7 @@ import { Markdown } from "@/lib/markdown";
 import { getAllPosts, getPostAndMorePosts } from "@/lib/api";
 
 export async function generateStaticParams() {
-  const allPosts = await getAllPosts(true);
+  const allPosts = await getAllPosts(false);
 
   return allPosts.map((post) => ({
     slug: post.slug,
@@ -23,7 +23,7 @@ export default async function PostPage({
   params: { slug: string };
 }) {
   const { isEnabled } = draftMode();
-  const { post, morePosts } = await getPostAndMorePosts(params.slug, true);
+  const { post, morePosts } = await getPostAndMorePosts(params.slug, isEnabled);
 
   return (
     <div className="container mx-auto px-5">
